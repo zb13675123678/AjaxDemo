13b31c462ea0bd71fe198d4230c803058007a2e9
zhangbo <1048985901@qq.com>
2020-04-21 10:08:41 +0800

    4，添加了百度搜索（原理：异步发送关键字获取数据）；

---
+++ src/main/java/com/qfedu/controller/SearchServlet.java
@@ -0,0 +1,58 @@
+package com.qfedu.controller;
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Administrator
+ */
+@WebServlet(name = "SearchServlet", urlPatterns = "/SearchServlet")
+public class SearchServlet extends HttpServlet {
+
+    private static List<String> datas = Arrays.asList("java", "javascript", "jsp", "aaa", "abc", "bbb");
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String keyword = request.getParameter("keyword");
+
+        System.out.println(keyword);
+        List<String> list = getResult(keyword);
+
+        Gson gson = new Gson();
+
+        response.getWriter().write(gson.toJson(list));
+    }
+
+    private List<String> getResult(String keyword) {
+        List<String> list = new ArrayList<>();
+
+        if (keyword == null || keyword.isEmpty()) {
+            return list;
+        }
+
+        for (String d : datas) {
+            if (d.contains(keyword)) {
+                list.add(d);
+            }
+        }
+
+        //Collections.sort(list);
+
+        return list;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+}
